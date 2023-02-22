2049692cb6bfd335633ccb55ac16b5b8e4b256c3
alexis-dimas <alexisdimas81@gmail.com>
2023-02-22 14:35:44 -0800

    Update rough draft for chart 2

--- chart2.R
+++ chart2.R
@@ -1,1 +1,18 @@
+# Most popular genres that emerged from the pandemic (by month)
+# Non-Fiction vs Fiction
 
+spl_10_df <- read.csv("2017-2023-10-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
+
+spl_10_df <- spl_10_df %>% mutate(date = paste0(CheckoutYear, "-", CheckoutMonth, "-01"))
+spl_10_df$date <- as.Date(spl_10_df$date, format = "%Y-%m-%d")
+
+nonfiction_checkouts <- spl_10_df %>% filter(str_detect(Subjects, "Nonfiction")) %>% group_by(date) %>% summarise(sum_checkouts = sum(Checkouts, na.rm = TRUE))
+
+fiction_checkouts <- spl_10_df %>% filter(str_detect(Subjects, "Fiction")) %>% group_by(date) %>% summarise(sum_checkouts = sum(Checkouts, na.rm = TRUE))
+
+ggplot() + 
+  geom_line(data = nonfiction_checkouts, aes(x = date, y = sum_checkouts), color = "blue") + 
+  geom_line(data = fiction_checkouts, aes(x = date, y = sum_checkouts), color = "red") + 
+  labs(title = "Nonfiction vs Fiction Checkouts 2017-2023",
+       x = "Date",
+       y = "Number of checkouts")
