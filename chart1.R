0e377556f78abf989f36e7e5a5ce2e55fc8fe22c
alexis-dimas <alexisdimas81@gmail.com>
2023-02-21 20:45:24 -0800

    Add data frames for CSV files

--- chart1.R
+++ chart1.R
@@ -1,1 +1,11 @@
+# Ideas
+
+spl_10_df <- read.csv("https://github.com/info-201a-wi23/a3-spl-checkouts-alexis-dimas/blob/main/2017-2023-10-Checkouts-SPL-Data.csv?raw=true", stringsAsFactors = FALSE)
+
+
+spl_All_df <- read.csv("https://github.com/info-201a-wi23/a3-spl-checkouts-alexis-dimas/blob/main/2022-2023-All-Checkouts-SPL-Data.csv?raw=true", stringsAsFactors = FALSE)
+
+
+spl_5_df <- read.csv("https://github.com/info-201a-wi23/a3-spl-checkouts-alexis-dimas/blob/main/2013-2023-5-Checkouts-SPL.csv?raw=true", stringsAsFactors = FALSE)
+
 
