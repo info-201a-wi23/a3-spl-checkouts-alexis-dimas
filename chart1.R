4ee665e0cc99083d21e5e6c50d1c74a55f55c767
alexis-dimas <alexisdimas81@gmail.com>
2023-02-22 14:35:18 -0800

    Update rough draft for chart 1

--- chart1.R
+++ chart1.R
@@ -1,11 +1,41 @@
 # Ideas
+library("dplyr")
+library("stringr")
+library("ggplot2")
 
-spl_10_df <- read.csv("https://github.com/info-201a-wi23/a3-spl-checkouts-alexis-dimas/blob/main/2017-2023-10-Checkouts-SPL-Data.csv?raw=true", stringsAsFactors = FALSE)
+# Trends from the COVID-19 Pandemic
 
 
-spl_All_df <- read.csv("https://github.com/info-201a-wi23/a3-spl-checkouts-alexis-dimas/blob/main/2022-2023-All-Checkouts-SPL-Data.csv?raw=true", stringsAsFactors = FALSE)
+spl_10_df <- read.csv("2017-2023-10-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
+spl_5_df <- read.csv("2013-2023-5-Checkouts-SPL.csv", stringsAsFactors = FALSE)
+spl_All_df <- read.csv("2022-2023-All-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
+
+spl_10_df <- spl_10_df %>% mutate(date = paste0(CheckoutYear, "-", CheckoutMonth, "-01"))
+spl_10_df$date <- as.Date(spl_10_df$date, format = "%Y-%m-%d")
 
 
-spl_5_df <- read.csv("https://github.com/info-201a-wi23/a3-spl-checkouts-alexis-dimas/blob/main/2013-2023-5-Checkouts-SPL.csv?raw=true", stringsAsFactors = FALSE)
 
+orwell_df <- spl_10_df %>% filter(str_detect(Creator, "George Orwell"))
+
+checkouts_per_month <- orwell_df %>% group_by(date) %>% summarise(sum_checkouts = sum(Checkouts, na.rm = TRUE))
+
+book_1984_checkouts <- orwell_df %>% filter(str_detect(Title, "1984")) %>% group_by(date) %>% summarise(sum_checkouts = sum(Checkouts, na.rm = TRUE))
+book_AF_checkouts <- orwell_df %>% filter(str_detect(Title, "Animal Farm")) %>% group_by(date) %>% summarise(sum_checkouts = sum(Checkouts, na.rm = TRUE))
+
+
+ggplot() + 
+  geom_line(data = book_1984_checkouts, aes(x = date, y = sum_checkouts), color = "blue") + 
+  geom_line(data = book_AF_checkouts, aes(x = date, y = sum_checkouts), color = "red") + 
+  labs(title = "1984 vs Animal Farm Checkouts 2017-2023",
+       x = "Date",
+       y = "Number of checkouts")
+
+
+
+
+ggplot(data = checkouts_per_month, aes(x = date, y = sum_checkouts)) + 
+  geom_line() + 
+  labs(title = "George Orwell Checkouts 2022-2023",
+       x = "Date",
+       y = "Number of checkouts")
 
