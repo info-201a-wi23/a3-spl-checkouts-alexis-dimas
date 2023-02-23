59adef597c0041bb1d826aab57a09d6e80118fe1
alexis-dimas <alexisdimas81@gmail.com>
2023-02-22 23:09:33 -0800

    Update rough draft chart1

--- chart1.R
+++ chart1.R
@@ -1,41 +1,30 @@
-# Ideas
+# Most popular genres that emerged from the pandemic (by month)
+# Non-Fiction vs Fiction
+
+# Load libraries
 library("dplyr")
 library("stringr")
 library("ggplot2")
 
-# Trends from the COVID-19 Pandemic
-
-
+# Data frames
 spl_10_df <- read.csv("2017-2023-10-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
-spl_5_df <- read.csv("2013-2023-5-Checkouts-SPL.csv", stringsAsFactors = FALSE)
-spl_All_df <- read.csv("2022-2023-All-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
 
+# Add to data frames
 spl_10_df <- spl_10_df %>% mutate(date = paste0(CheckoutYear, "-", CheckoutMonth, "-01"))
+
 spl_10_df$date <- as.Date(spl_10_df$date, format = "%Y-%m-%d")
 
+# Variables
+nf_checkouts <- spl_10_df %>% filter(str_detect(Subjects, "Nonfiction")) %>% group_by(date) %>% summarise(nonfiction_checkouts = sum(Checkouts, na.rm = TRUE))
 
+f_checkouts <- spl_10_df %>% filter(str_detect(Subjects, "Fiction")) %>% group_by(date) %>% summarise(fiction_checkouts = sum(Checkouts, na.rm = TRUE))
 
-orwell_df <- spl_10_df %>% filter(str_detect(Creator, "George Orwell"))
-
-checkouts_per_month <- orwell_df %>% group_by(date) %>% summarise(sum_checkouts = sum(Checkouts, na.rm = TRUE))
-
-book_1984_checkouts <- orwell_df %>% filter(str_detect(Title, "1984")) %>% group_by(date) %>% summarise(sum_checkouts = sum(Checkouts, na.rm = TRUE))
-book_AF_checkouts <- orwell_df %>% filter(str_detect(Title, "Animal Farm")) %>% group_by(date) %>% summarise(sum_checkouts = sum(Checkouts, na.rm = TRUE))
-
-
+# Plot 
 ggplot() + 
-  geom_line(data = book_1984_checkouts, aes(x = date, y = sum_checkouts), color = "blue") + 
-  geom_line(data = book_AF_checkouts, aes(x = date, y = sum_checkouts), color = "red") + 
-  labs(title = "1984 vs Animal Farm Checkouts 2017-2023",
+  geom_line(data = nf_checkouts, aes(x = date, y = nonfiction_checkouts, color = "Non-fiction")) + 
+  geom_line(data = f_checkouts, aes(x = date, y = fiction_checkouts, color = "Fiction")) + 
+  scale_color_manual(values=c("#0000ff", "#ff0000")) + 
+  labs(title = "Non-fiction vs Fiction Checkouts 2017-2023",
        x = "Date",
-       y = "Number of checkouts")
-
-
-
-
-ggplot(data = checkouts_per_month, aes(x = date, y = sum_checkouts)) + 
-  geom_line() + 
-  labs(title = "George Orwell Checkouts 2022-2023",
-       x = "Date",
-       y = "Number of checkouts")
-
+       y = "Number of checkouts",
+       color = "Legend")
