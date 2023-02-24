8e0e61b3f1da391dcd6d93311d20395db71832dc
alexis-dimas <alexisdimas81@gmail.com>
2023-02-24 09:50:03 -0800

    Complete chart1 final draft

--- chart1.R
+++ chart1.R
@@ -1,30 +1,38 @@
-# Most popular genres that emerged from the pandemic (by month)
-# Non-Fiction vs Fiction
+# Non-Fiction and Fiction Checkouts In The Past Six Years
 
 # Load libraries
 library("dplyr")
 library("stringr")
 library("ggplot2")
+library("scales")
 
-# Data frames
+# Data frame (At least 10 checkouts a month in the past six years)
 spl_10_df <- read.csv("2017-2023-10-Checkouts-SPL-Data.csv", stringsAsFactors = FALSE)
 
-# Add to data frames
-spl_10_df <- spl_10_df %>% mutate(date = paste0(CheckoutYear, "-", CheckoutMonth, "-01"))
-
+# Add date to data frames
+spl_10_df <- spl_10_df %>% 
+  mutate(date = paste0(CheckoutYear, "-", CheckoutMonth, "-01"))
 spl_10_df$date <- as.Date(spl_10_df$date, format = "%Y-%m-%d")
 
-# Variables
-nf_checkouts <- spl_10_df %>% filter(str_detect(Subjects, "Nonfiction")) %>% group_by(date) %>% summarise(nonfiction_checkouts = sum(Checkouts, na.rm = TRUE))
+# Number of non-fiction (nf) checkouts each month in the past six years
+nf_checkouts <- spl_10_df %>% 
+  filter(str_detect(Subjects, "Nonfiction")) %>% 
+  group_by(date) %>% 
+  summarise(nonfiction_checkouts = sum(Checkouts, na.rm = TRUE))
 
-f_checkouts <- spl_10_df %>% filter(str_detect(Subjects, "Fiction")) %>% group_by(date) %>% summarise(fiction_checkouts = sum(Checkouts, na.rm = TRUE))
+# Number of fiction (f) checkouts each month in the past six years
+f_checkouts <- spl_10_df %>% 
+  filter(str_detect(Subjects, "Fiction")) %>% 
+  group_by(date) %>% 
+  summarise(fiction_checkouts = sum(Checkouts, na.rm = TRUE))
 
-# Plot 
+# Line plot
 ggplot() + 
   geom_line(data = nf_checkouts, aes(x = date, y = nonfiction_checkouts, color = "Non-fiction")) + 
   geom_line(data = f_checkouts, aes(x = date, y = fiction_checkouts, color = "Fiction")) + 
   scale_color_manual(values=c("#0000ff", "#ff0000")) + 
-  labs(title = "Non-fiction vs Fiction Checkouts 2017-2023",
+  scale_y_continuous(labels = label_number_si()) + 
+  labs(title = "Non-fiction and Fiction Checkouts In The Past Six Years",
        x = "Date",
-       y = "Number of checkouts",
+       y = "Number of Checkouts",
        color = "Legend")
